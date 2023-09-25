094a00829fecd318e3718b7ecf35c8e21bdb35dc
ypww <rororo3434@yhoo.co.jp>
2023-09-25 18:02:45 +0900

    AC

--- ABC283/F.cpp
+++ ABC283/F.cpp
@@ -19,9 +19,39 @@ template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, tr
 const double pi = 3.141592653589793;
 const ll INF = (ll)1e15;
 
+ll op(ll a, ll b){ return min(a, b); };
+ll e(){ return INF; };
+
 int main()
 {
-    adjacent_difference
-    getloadavg
+    int n;
+    cin >> n;
+    vector<int>p(n+1);
+    rep(i,1,n+1)cin>>p[i];
+
+    segtree<ll,op,e> seg1(n+1), seg2(n+1), seg3(n+1), seg4(n+1);
+    rep(i,1,n+1){
+        seg1.set(p[i], p[i]+i);
+        seg2.set(p[i], i-p[i]);
+    }
+    vector<ll> ans(n+1);
+    rep(i,1,n+1){
+        ll res = INF;
+        seg1.set(p[i], INF);
+        seg2.set(p[i], INF);
+        chmin(res, -(p[i]+i) + seg1.prod(p[i]+1, n+1));
+        chmin(res, p[i]-i + seg2.prod(0, p[i]));
+        chmin(res, -p[i]+i + seg3.prod(p[i]+1, n+1));
+        chmin(res, p[i]+i + seg4.prod(0, p[i]));
+        seg3.set(p[i], p[i]-i);
+        seg4.set(p[i], -p[i]-i);
+        ans[i] = res;
+    }
+    rep(i,1,n+1){
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+
+
     return 0;
 }
