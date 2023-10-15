97b40c93153a85171854ecab98c0c4204d1ef665
ypww <rororo3434@yhoo.co.jp>
2023-10-16 07:36:24 +0900

    AC

---
+++ tessoku-book/cpp/A28.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ull = unsigned long long;
+
+#define rep(i, a, b) for(ll i=a; i<b; i++)
+#define rrep(i, a, b) for(ll i=a; i>=b; i--)
+#define all(a) (a).begin(), (a).end()
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+
+const double pi = 3.141592653589793;
+const ll INF = (ll)1e15;
+
+int main()
+{
+    ll n;
+    cin>>n;
+    ll mod = 10000;
+    ll ans = 0;
+    while (n){
+        n--;
+        char t;
+        ll a;
+        cin>>t>>a;
+        if (t=='+'){
+            ans += a;
+        }else if(t=='-'){
+            ans -= a;
+        }else{
+            ans *= a;
+        }
+        ans %= mod;
+        ans += mod;
+        ans %= mod;
+        cout << ans << endl;
+    }
+    
+    return 0;
+}
