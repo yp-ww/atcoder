56720725b982de5ddbbd5e54a2b1dd3e4483ba3b
ypww <rororo3434@yhoo.co.jp>
2023-11-11 20:34:31 +0900

    AC

---
+++ ABC326/B.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ull = unsigned long long;
+
+#define rep(i, a, b) for(ll i=a; i<b; i++)
+#define rrep(i, a, b) for(ll i=a; i>=b; i--)
+#define all(a) (a).begin(), (a).end()
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+
+const double pi = 3.141592653589793;
+const ll INF = (ll)1e18+10;
+
+int main()
+{
+    ll n;
+    cin>>n;
+    while (1){
+        bool flag = false;
+        ll x = (n/100);
+        ll y = (n/10)%10;
+        ll z = n%10;
+        if (x*y==z)flag = true;
+        if (flag)break;
+        n++;
+    }
+    cout << n << endl;
+    return 0;
+}
