da4dca55e6e885e748685024fa20219e8e4d12f2
ypww <rororo3434@yhoo.co.jp>
2023-11-17 10:25:00 +0900

    AC

---
+++ asakatsu20231117/A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ull = unsigned long long;
+
+#define rep(i, a, b) for(ll i=a; i<b; i++)
+#define rrep(i, a, b) for(ll i=a; i>=b; i--)
+#define all(a) (a).begin(), (a).end()
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+
+const double pi = 3.141592653589793;
+const ll INF = (ll)1e18+10;
+
+int main()
+{
+    ll a,b,c;
+    cin>>a>>b>>c;
+    vector<ll>arr;
+    arr.push_back(a);
+    arr.push_back(b);
+    arr.push_back(c);
+    sort(all(arr));
+    bool flag;
+    if (arr[0]+arr[1]==arr[2])flag = true;
+    else flag = false;
+    YesNo(flag);
+    return 0;
+}
---
+++ asakatsu20231117/C.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ull = unsigned long long;
+
+#define rep(i, a, b) for(ll i=a; i<b; i++)
+#define rrep(i, a, b) for(ll i=a; i>=b; i--)
+#define all(a) (a).begin(), (a).end()
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+
+const double pi = 3.141592653589793;
+const ll INF = (ll)1e18+10;
+
+int main()
+{
+    ll m,n,N;
+    cin>>m>>n>>N;
+    ll ans = 0;
+    ll rem = 0;
+    while(N){
+        ans += N;
+        ll nex = (N+rem)/m;
+        rem = (N+rem)%m;
+        N = nex*n;
+    }
+    cout << ans << endl;
+    return 0;
+}
