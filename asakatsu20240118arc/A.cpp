3c24f7e158f0d1fd6c9dc7b89c238d858b4feffc
ypww <rororo3434@yhoo.co.jp>
2024-01-18 19:52:37 +0900

    AC

---
+++ asakatsu20240118arc/A.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ull = unsigned long long;
+template <class T> using priority_queue_rev = priority_queue<T, vector<T>, greater<T>>;
+
+#define rep(i, a, b) for(ll i=a; i<b; i++)
+#define rrep(i, a, b) for(ll i=a; i>=b; i--)
+#define all(a) (a).begin(), (a).end()
+#define smod(n, m) ((((n) % (m)) + (m)) % (m)) // 非負mod
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+
+inline int popcount(int n) { return __builtin_popcount(n); } // 2進数で表した場合に立ってるビット数がいくつか
+inline int popcount(ll n) { return __builtin_popcountll(n); }
+inline int ctz(int n) { return n != 0 ? __builtin_ctz(n) : -1; } // 2進数で表した場合に 1 の位からいくつ 0 が連なっているか
+inline int ctz(ll n) { return n != 0 ? __builtin_ctzll(n) : -1; }
+inline int clz(int n) { return n != 0 ? (31 - __builtin_clz(n)) : -1; } // 2進数で表した場合に左側にいくつ 0 を埋める必要があるか
+inline int clz(ll n) { return n != 0 ? (63 - __builtin_clzll(n)) : -1; }
+
+const double PI = 3.141592653589793;
+const vector<int> DX = { 1, 0, -1, 0 };
+const vector<int> DY = { 0, 1, 0, -1 };
+const long long INF = (ll)1e18+10;
+ll coordinate(ll h, ll w, ll W){ return h*W + w; } // 二次元座標を一次元座標に変換
+
+int main()
+{
+    ll n;
+    cin>>n;
+    set<ll>st;
+    rep(a,1,10)rep(b1,0,10)rep(b2,0,10)rep(c,0,10)rep(d,0,10)rep(e,0,10){
+        string s = "";
+        s += char('0'+a);
+        s += char('0'+a);
+        s += char('0'+b1); 
+        s += char('0'+b2); 
+        s += char('0'+c);
+        s += char('0'+c);
+        s += char('0'+d); 
+        s += char('0'+e); 
+        s += char('0'+d);
+        st.insert(stoll(s));
+    }
+    auto itr = st.begin();
+    while(--n){
+        itr++;
+    }
+    cout << *itr << endl;
+    
+    // cout << fixed << setprecision(18);
+    return 0;
+}
