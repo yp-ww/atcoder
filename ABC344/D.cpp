b630a31ec93c9e1ef3e043572e6cf9c840fe73be
ypww <rororo3434@yhoo.co.jp>
2024-03-10 00:28:02 +0900

    AC

---
+++ ABC344/D.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ull = unsigned long long;
+template <class T> using priority_queue_rev = priority_queue<T, vector<T>, greater<T>>;
+
+#define rep(i, a, b) for(ll i=a; i<b; i++)
+#define rrep(i, a, b) for(ll i=a; i>=b; i--)
+#define all(a) (a).begin(), (a).end()
+#define smod(n, m) ((((n) % (m)) + (m)) % (m)) // 非負mod
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+
+inline int popcount(int n) { return __builtin_popcount(n); } // 2進数で表した場合に立ってるビット数がいくつか
+inline int popcount(ll n) { return __builtin_popcountll(n); }
+inline int ctz(int n) { return n != 0 ? __builtin_ctz(n) : -1; } // 2進数で表した場合に 1 の位からいくつ 0 が連なっているか
+inline int ctz(ll n) { return n != 0 ? __builtin_ctzll(n) : -1; }
+inline int clz(int n) { return n != 0 ? (31 - __builtin_clz(n)) : -1; } // 2進数で表した場合に左側にいくつ 0 を埋める必要があるか
+inline int clz(ll n) { return n != 0 ? (63 - __builtin_clzll(n)) : -1; }
+
+const double PI = 3.141592653589793;
+const vector<int> DX = { 1, 0, -1, 0 };
+const vector<int> DY = { 0, 1, 0, -1 };
+const long long INF = 4004004003104004004LL; // (int)INF = 1010931620;
+ll coordinate(ll h, ll w, ll W){ return h*W + w; } // 二次元座標を一次元座標に変換
+
+#define endl "\n" // インタラクティブの時はコメントアウトする
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    // cout << fixed << setprecision(18);
+    
+    string t;
+    cin>>t;
+    ll n;
+    cin>>n;
+    vector<vector<string>> arr(n);
+    rep(i,0,n){
+        ll a;
+        cin>>a;
+        rep(j,0,a){
+            string s;
+            cin>>s;
+            arr[i].push_back(s);
+        }
+    }
+
+    vector<vector<ll>> dp(n+1,vector<ll>(t.size()+1, INF));
+    dp[0][0] = 0;
+    rep(i,0,n)rep(j,0,t.size()+1){
+        if (dp[i][j]==INF)continue;
+        chmin(dp[i+1][j], dp[i][j]);
+        for (auto s: arr[i]){
+            ll sz = s.size();
+            string tt;
+            if (j+sz>t.size()) continue;
+            rep(k,j,j+sz) tt += t[k];
+            if (tt==s){
+                chmin(dp[i+1][j+sz], dp[i][j]+1);
+            }
+        }
+    }
+    if (dp[n][t.size()]==INF) cout << -1 << endl;
+    else cout << dp[n][t.size()] << endl;
+    return 0;
+}
---
+++ ABC344/E.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using ull = unsigned long long;
+template <class T> using priority_queue_rev = priority_queue<T, vector<T>, greater<T>>;
+
+#define rep(i, a, b) for(ll i=a; i<b; i++)
+#define rrep(i, a, b) for(ll i=a; i>=b; i--)
+#define all(a) (a).begin(), (a).end()
+#define smod(n, m) ((((n) % (m)) + (m)) % (m)) // 非負mod
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+
+inline int popcount(int n) { return __builtin_popcount(n); } // 2進数で表した場合に立ってるビット数がいくつか
+inline int popcount(ll n) { return __builtin_popcountll(n); }
+inline int ctz(int n) { return n != 0 ? __builtin_ctz(n) : -1; } // 2進数で表した場合に 1 の位からいくつ 0 が連なっているか
+inline int ctz(ll n) { return n != 0 ? __builtin_ctzll(n) : -1; }
+inline int clz(int n) { return n != 0 ? (31 - __builtin_clz(n)) : -1; } // 2進数で表した場合に左側にいくつ 0 を埋める必要があるか
+inline int clz(ll n) { return n != 0 ? (63 - __builtin_clzll(n)) : -1; }
+
+const double PI = 3.141592653589793;
+const vector<int> DX = { 1, 0, -1, 0 };
+const vector<int> DY = { 0, 1, 0, -1 };
+const long long INF = 4004004003104004004LL; // (int)INF = 1010931620;
+ll coordinate(ll h, ll w, ll W){ return h*W + w; } // 二次元座標を一次元座標に変換
+
+#define endl "\n" // インタラクティブの時はコメントアウトする
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    // cout << fixed << setprecision(18);
+    
+    ll n;
+    cin>>n;
+    vector<ll>a(n+2);
+    rep(i,1,n+1)cin>>a[i];
+    ll q;
+    cin>>q;
+    map<ll,ll> pre;
+    map<ll,ll> nex;
+
+    a[0] = -INF;
+    a[n+1] = INF;
+    rep(i,1,n+1){
+        pre[a[i]] = a[i-1];
+        nex[a[i]] = a[i+1];
+    }
+    nex[a[0]] = a[1];
+    pre[a[n+1]] = a[n];
+
+    while(q--){
+        ll c;
+        cin>>c;
+        if (c==1){
+            ll x,y;
+            cin>>x>>y;
+            ll nownex = nex[x];
+            nex[x] = y;
+            pre[nownex] = y;
+            pre[y] = x;
+            nex[y] = nownex;
+        }else{
+            ll x;
+            cin>>x;
+            ll nowpre = pre[x];
+            ll nownex = nex[x];
+            nex[nowpre] = nownex;
+            pre[nownex] = nowpre;
+            pre[x] = -1;
+            nex[x] = -1;
+        }
+    }
+    ll now = a[0];
+    vector<ll> ans;
+    while(now!=INF){
+        ll aa = nex[now];
+        ans.push_back(aa);
+        now = aa;
+    }
+    ans.pop_back();
+    for (auto aa: ans) cout << aa << endl;
+
+    return 0;
+}
